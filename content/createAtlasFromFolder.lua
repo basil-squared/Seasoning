a8e27dceca57285b67d6970f855ac64b664d3f61
Juniper <basil.squared1@gmail.com>
2026-04-23 08:55:11 -0500

    start working on this function

---
+++ content/createAtlasFromFolder.lua
@@ -0,0 +1,13 @@
+--[[
+    Given an assets folder, take all sprite images within
+    and auto configure them based on the naming scheme of the file.
+
+    Format should go ~something like this
+    [NAME_OF_ATLAS]_PX_PY_[ANIM/NORM]
+
+]]
+
+
+
+function SEAS.FUNC.CreateAtlasFromFolder(folder)
+end
